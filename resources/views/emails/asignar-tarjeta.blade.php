27979c3358a732ff1c49fef48e7282716b13fafb
Kevin <kavx1001@gmail.com>
2018-06-08 22:46:34 -0600

    cambios_kevin

--- resources/views/emails/asignar-tarjeta.blade.php
+++ resources/views/emails/asignar-tarjeta.blade.php
@@ -1,6 +1,12 @@
 @component('mail::message')
 
-Estimad@ <strong>{{$tarjeta->asignado->name}}</strong> se le ha asignado esta tarjeta:
+
+
+Estimad@ <strong>@if(isset($tarjeta->user_reasignado))
+            {{$tarjeta->reasignado->name}} 
+          @else {{$tarjeta->asignado->name}}                   
+          @endif
+</strong> se le ha asignado esta tarjeta:
 <br>
 @component('mail::panel')
 <h5><strong>Numero: </strong><span>{{$tarjeta->id}} </span></h5>
@@ -9,6 +15,9 @@ Estimad@ <strong>{{$tarjeta->asignado->name}}</strong> se le ha asignado esta ta
 <h5><strong>Planta: </strong><span class="blue">{{$tarjeta->planta->nombre}} </span></h5>
 <h5><strong>Prioridad: </strong><span class="blue">{{$tarjeta->prioridad}} </span></h5>
 <h5><strong>Descripcion del Reporte: </strong><span class="blue">{{$tarjeta->descripcion_reporte}}</span> </h5>
+@if(isset($tarjeta->user_reasignado))
+<h5><strong>Motivo: </strong><span class="blue">{{$tarjeta->motivo_reasignado}}</span> </h5>
+@endif 
 @endcomponent
 
 @component('mail::button', ['url' => 'http://digital-cic.com/tarjetas/'.$tarjeta->id])
@@ -18,3 +27,4 @@ Ver Tarjeta
 <br>
 Sistema de Tarjetas CIC
 @endcomponent
+  
